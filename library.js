e3e50fb0bb3bb5de64f64aeda91d60139d72b20a
Saatvik <saatvik.jbp@gmail.com>
2023-10-29 19:14:27 +0530

    Adding function is in process

--- library.js
+++ library.js
@@ -5,13 +5,6 @@ const cancelBtn = document.querySelector('.cancelButton');
 const authorBox = document.querySelector('#book-author');
 const titleBox = document.querySelector('#book-title');
 
-const div = document.createElement('div');
-div.setAttribute('class', 'book-container');
-const autherParagraph = document.createElement('p');autherParagraph.setAttribute('class', 'author');
-const titleParagraph = document.createElement('p');
-titleParagraph.setAttribute('class', 'title');
-div.append(titleParagraph, autherParagraph);
-
 const myLibrary = [];
 
 function Book(title, author){
@@ -22,33 +15,25 @@ function Book(title, author){
 function addBookToLibrary(newBook){
     //if array is empty then place that book
     //else check if it contains book or not
-        //if containes the book then dont add it
-        //else add that book
     if(myLibrary.length == 0){
         addToDOM(newBook);
         myLibrary.push(newBook);
     }
     else {
-        myLibrary.forEach((book) => {
-            if (newBook.title != book.title){
-                myLibrary.push(newBook);
-                addToDOM(newBook);
-            }
-            else{
-                console.log('already exist')
-            }
-        })
+        //go through each book
+        //if book contains
     }
 }
 
 function addToDOM(newBook){
     const div = document.createElement('div');
     div.setAttribute('class', 'book-container');
-    const autherParagraph = document.createElement('p');autherParagraph.setAttribute('class', 'author');
+    const autherParagraph = document.createElement('p');
+    autherParagraph.setAttribute('class', 'author');
     const titleParagraph = document.createElement('p');
     titleParagraph.setAttribute('class', 'title');
     div.append(titleParagraph, autherParagraph);
-    titleParagraph.innerText = 'Name: '+ newBook.title;
+    titleParagraph.innerText = 'Title: '+ newBook.title;
     autherParagraph.innerText = 'Author: '+ newBook.author;
     document.body.appendChild(div);
 }
