377c429dee50d1abce03a3c0cb3a85d1bc75bf48
Saatvik <saatvik.jbp@gmail.com>
2023-10-30 22:45:32 +0530

    Added done ✅ and reading status in progress

--- library.js
+++ library.js
@@ -4,12 +4,24 @@ const confirmBtn = document.querySelector('.confirmButton');
 const cancelBtn = document.querySelector('.cancelButton');
 const authorBox = document.querySelector('#book-author');
 const titleBox = document.querySelector('#book-title');
+const pagesBox = document.querySelector('#book-pages');
+const noBox = document.querySelectorAll('input[name="Reading-Status"]');
 
 const myLibrary = [];
 
-function Book(title, author){
+function Book(title, author, pages){
     this.title = title;
     this.author = author;
+    this.pages = pages;
+}
+
+Book.prototype.ReadStatus = function () {
+    for (const radioButton of noBox){
+        if (radioButton.checked){
+            this.read = radioButton.value;
+            console.log(this.read)
+        }
+    }
 }
 
 function addBookToLibrary(newBook){
@@ -20,21 +32,35 @@ function addBookToLibrary(newBook){
         myLibrary.push(newBook);
     }
     else {
-        //go through each book
-        //if book contains
+        for (const index in myLibrary){
+            if (myLibrary[index].title.toLowerCase() !== newBook.title.toLowerCase() && myLibrary.indexOf(myLibrary[index]) == myLibrary.length - 1){
+                addToDOM(newBook);
+                myLibrary.push(newBook);
+            }
+            else if (myLibrary[index].title.toLowerCase() == newBook.title.toLowerCase()){
+                console.log("already exist");
+                break;
+            }
+        }
     }
 }
-
 function addToDOM(newBook){
     const div = document.createElement('div');
     div.setAttribute('class', 'book-container');
+
     const autherParagraph = document.createElement('p');
     autherParagraph.setAttribute('class', 'author');
+
     const titleParagraph = document.createElement('p');
     titleParagraph.setAttribute('class', 'title');
-    div.append(titleParagraph, autherParagraph);
-    titleParagraph.innerText = 'Title: '+ newBook.title;
-    autherParagraph.innerText = 'Author: '+ newBook.author;
+    
+    const noOfPages = document.createElement('p');
+    noOfPages.setAttribute('class', 'pages');
+
+    titleParagraph.innerText = 'Title: ' + newBook.title;
+    autherParagraph.innerText = 'Author: ' + newBook.author;
+    noOfPages.innerText = 'Pages: ' + newBook.pages;
+    div.append(titleParagraph, autherParagraph, noOfPages);
     document.body.appendChild(div);
 }
 
@@ -51,9 +77,9 @@ cancelBtn.addEventListener('click', (e) => {
 //When the confirm book button is clicked
 //new book is created and displayed in new tab
 confirmBtn.addEventListener('click', (event) => {
-        let newBook = new Book(titleBox.value, authorBox.value);
+        let newBook = new Book(titleBox.value, authorBox.value, pagesBox.value);
         if(titleBox.value != '' && authorBox.value != ''){
-            addBookToLibrary(newBook)
+            addBookToLibrary(newBook);
         }
         else {
             event.preventDefault();
